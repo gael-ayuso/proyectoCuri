6dbd98280bd7048017cee8101ed2c6b87dbd3a05
Gael Ayuso <a21201424@alumnos.uady.mx>
2026-05-14 21:11:24 -0600

    Initial commit

---
+++ composeApp/src/jvmTest/kotlin/compose/project/proyectocuri/ComposeAppDesktopTest.kt
@@ -0,0 +1,12 @@
+package compose.project.proyectocuri
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppDesktopTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
