06e280726bf36f68071c2fdb5b65bc3c9f3d7291
CristopherBv <a21201372@alumnos.uady.mx>
2026-05-21 23:21:39 -0600

    Se adaptó el controller y se agregaron 2 funciones que faltaban

--- composeApp/src/jvmMain/kotlin/controllers/ContactoController.kt
+++ composeApp/src/jvmMain/kotlin/controllers/ContactoController.kt
@@ -2,25 +2,59 @@ package controllers
 
 import models.Contacto
 import models.ContactoDAO
+import models.ContactoDAOJSON
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
-class ContactoController: ContactoDAO {
+class ContactoController : ContactoDAO {
+    private val dao: ContactoDAO = ContactoDAOJSON()
+
     override fun create(contacto: Contacto) {
-        TODO("Not yet implemented")
+        dao.create(contacto)
     }
 
     override fun readByName(name: String): Contacto? {
-        TODO("Not yet implemented")
+        return dao.readByName(name)
     }
 
     override fun readAll(): List<Contacto> {
-        TODO("Not yet implemented")
+        return dao.readAll()
     }
 
     override fun update(contacto: Contacto) {
-        TODO("Not yet implemented")
+        dao.update(contacto)
     }
 
     override fun delete(contacto: Contacto) {
-        TODO("Not yet implemented")
+        dao.delete(contacto)
+    }
+
+    /**
+     *  Listar los contactos y su fecha de cumpleaños ordenados a partir del día actual.
+     */
+    fun obtenerContactosPorCumpleanosProximo(): List<Contacto> {
+        val today = LocalDate.now()
+
+        return dao.readAll().sortedBy { contacto ->
+            // Cambiamos el año del cumpleaños al año actual para comparar los diasw
+            val cumpleEsteAno = contacto.birthDate.withYear(today.year)
+
+            // Si el cumpleaños ya paso, calculamos los dias para el proximo año
+            val proximoCumple = if (cumpleEsteAno.isBefore(today)) {
+                cumpleEsteAno.plusYears(1)
+            } else {
+                cumpleEsteAno
+            }
+
+            // Devuelve los dias que faltan (orden de menor a mayor)
+            ChronoUnit.DAYS.between(today, proximoCumple)
+        }
+    }
+
+    /**
+     * Listar todos los contactos ordenados alfabéticamente por nombre.
+     */
+    fun obtenerContactosOrdenAlfabetico(): List<Contacto> {
+        return dao.readAll().sortedBy { it.name.lowercase() }
     }
 }
