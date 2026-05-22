10045c0798dcb94aa799956a470a4e54aa5cdc69
CristopherBv <a21201372@alumnos.uady.mx>
2026-05-21 21:57:20 -0600

    Contacto.kt se volvió una Data Class

--- composeApp/src/jvmMain/kotlin/models/Contacto.kt
+++ composeApp/src/jvmMain/kotlin/models/Contacto.kt
@@ -1,36 +1,19 @@
 package models
 
-import java.util.Date
+import java.time.LocalDate
 
-class Contacto {
-    private val name: String;
-    private val birthDate: Date;
-    private val phoneNumber: Int;
-    private val email: String;
-
-    constructor(name: String, birthDate: Date, phoneNumber: Int, email: String) {
-        this.name = name;
-        this.birthDate = birthDate;
-        this.phoneNumber = phoneNumber;
-
-        if (!Regex ("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$").matches(email)) {
-            throw Exception("Email no valido")
-        }else{
-            this.email = email;
+//Al ser un data class se "generan"
+// en automatico los setters y getters
+data class Contacto(
+    val name: String,
+    val birthDate: LocalDate,
+    val phoneNumber: String,
+    val email: String
+) {
+    init {
+        // El bloque init se ejecuta al crear la instancia. Aquí va tu validación.
+        if (!Regex("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$").matches(email)) {
+            throw IllegalArgumentException("Email no valido")
         }
     }
-
-    public fun getBirthDate(): Date {
-        return birthDate;
-    }
-    public fun getEmail(): String {
-        return email;
-    }
-    public fun getName(): String {
-        return name;
-    }
-    public fun getPhoneNumber(): Int {
-        return phoneNumber;
-    }
-
 }
