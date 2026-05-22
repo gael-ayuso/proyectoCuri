c04c90e9ac7e1674c7d3fa35211c6c914fb888b9
CristopherBv <a21201372@alumnos.uady.mx>
2026-05-22 00:26:44 -0600

    Se agrego el boton de agregar contacto en App.kt

--- composeApp/src/jvmMain/kotlin/compose/project/proyectocuri/App.kt
+++ composeApp/src/jvmMain/kotlin/compose/project/proyectocuri/App.kt
@@ -1,49 +1,34 @@
 package compose.project.proyectocuri
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.painterResource
-
-import proyectocuri.composeapp.generated.resources.Res
-import proyectocuri.composeapp.generated.resources.compose_multiplatform
+import androidx.compose.ui.unit.dp
+import controllers.ContactoController
+import models.Contacto
+import java.time.LocalDate
 
 @Composable
-@Preview
 fun App() {
+    val controller = remember { ContactoController() }
+
+    // Este estado controla si la zona de agregar contacto es visible o no
+    var mostrarFormulario by remember { mutableStateOf(false) }
+
+
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+        Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+            Button(
+                onClick = { mostrarFormulario = true },
+                modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
+            ) {
+                Text("Agregar contacto")
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+
         }
+
     }
 }
